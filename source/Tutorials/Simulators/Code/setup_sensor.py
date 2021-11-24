df7e361835fe09a325412dc7d2657adb7a7f1716
Benjamin Hug <benjamin.hug1997@gmail.com>
2021-11-24 12:27:36 +0100

    consistent string name in copied files for sensor

--- source/Tutorials/Simulators/Code/setup_sensor.py
+++ source/Tutorials/Simulators/Code/setup_sensor.py
@@ -5,7 +5,7 @@ data_files = []
 data_files.append(('share/ament_index/resource_index/packages', ['resource/' + package_name]))
 data_files.append(('share/' + package_name + '/launch', ['launch/robot_launch.py']))
 data_files.append(('share/' + package_name + '/worlds', ['worlds/my_world.wbt']))
-data_files.append(('share/' + package_name + '/resource', ['resource/my_robot_webots.urdf']))
+data_files.append(('share/' + package_name + '/resource', ['resource/my_robot.urdf']))
 data_files.append(('share/' + package_name, ['package.xml']))
 
 setup(
