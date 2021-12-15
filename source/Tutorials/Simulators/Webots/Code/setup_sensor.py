9f7165c94f2c2529dfb1b1f92b13c1b2bfa728dd
Olivier Michel <Olivier.Michel@cyberbotics.com>
2021-12-15 14:24:18 +0100

    New advanced tutorial: "Setting-up a Robot Simulation (Webots)" (backport #2156) (#4) (#2175)
    * Update Tutorials.rst

---
+++ source/Tutorials/Simulators/Webots/Code/setup_sensor.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+package_name = 'my_package'
+data_files = []
+data_files.append(('share/ament_index/resource_index/packages', ['resource/' + package_name]))
+data_files.append(('share/' + package_name + '/launch', ['launch/robot_launch.py']))
+data_files.append(('share/' + package_name + '/worlds', ['worlds/my_world.wbt']))
+data_files.append(('share/' + package_name + '/resource', ['resource/my_robot.urdf']))
+data_files.append(('share/' + package_name, ['package.xml']))
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=data_files,
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='user',
+    maintainer_email='user.name@mail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'my_robot_driver = my_package.my_robot_driver:main',
+            'obstacle_avoider = my_package.obstacle_avoider:main'
+        ],
+    },
+)
