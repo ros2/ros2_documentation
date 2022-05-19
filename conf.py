e17f677dbeaf4f557c27ed98bb60a72dcad30540
Audrow Nash <audrow@hey.com>
2022-05-18 17:10:28 -0700

    Change humble to the latest version
    Signed-off-by: Audrow Nash <audrow@hey.com>

--- conf.py
+++ conf.py
@@ -127,7 +127,7 @@ smv_branch_whitelist = r'^(rolling|humble|galactic|foxy|eloquent|dashing|crystal
 
 smv_released_pattern = r'^refs/(heads|remotes/[^/]+)/(humble|galactic|foxy|eloquent|dashing|crystal).*$'
 smv_remote_whitelist = r'^(origin)$'
-smv_latest_version = 'galactic'
+smv_latest_version = 'humble'
 smv_eol_versions = ['crystal', 'dashing', 'eloquent']
 
 distro_full_names = {
