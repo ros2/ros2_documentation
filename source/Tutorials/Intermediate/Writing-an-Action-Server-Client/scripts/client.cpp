ef44007b2327f02ef19af059d9cbd510470d7291
0ljik <oljaktl@gmail.com>
2022-06-13 20:43:03 +0600

    update goal_response_callback for humble

--- source/Tutorials/Intermediate/Writing-an-Action-Server-Client/scripts/client.cpp
+++ source/Tutorials/Intermediate/Writing-an-Action-Server-Client/scripts/client.cpp
@@ -60,9 +60,8 @@ private:
   rclcpp_action::Client<Fibonacci>::SharedPtr client_ptr_;
   rclcpp::TimerBase::SharedPtr timer_;
 
-  void goal_response_callback(std::shared_future<GoalHandleFibonacci::SharedPtr> future)
+  void goal_response_callback(const GoalHandleFibonacci::SharedPtr & goal_handle)
   {
-    auto goal_handle = future.get();
     if (!goal_handle) {
       RCLCPP_ERROR(this->get_logger(), "Goal was rejected by server");
     } else {
