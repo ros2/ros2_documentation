9fe05978756fab405ab0984fe0456a5b171e6005
Yannick Goumaz <61198661+ygoumaz@users.noreply.github.com>
2023-05-22 14:00:41 +0200

    Adding more Webots Simulation Tutorials (#3357)
    Refactor the Webots tutorials as well.
    
    Co-authored-by: ad-daniel <44834743+ad-daniel@users.noreply.github.com>
    Co-authored-by: Olivier Michel <Olivier.Michel@cyberbotics.com>
    Co-authored-by: Chris Lalancette <clalancette@gmail.com>

---
+++ source/Tutorials/Advanced/Simulators/Webots/Code/MyRobotDriver.cpp
@@ -0,0 +1,55 @@
+#include "my_package/MyRobotDriver.hpp"
+
+#include "rclcpp/rclcpp.hpp"
+#include <cstdio>
+#include <functional>
+#include <webots/motor.h>
+#include <webots/robot.h>
+
+#define HALF_DISTANCE_BETWEEN_WHEELS 0.045
+#define WHEEL_RADIUS 0.025
+
+namespace my_robot_driver {
+void MyRobotDriver::init(
+    webots_ros2_driver::WebotsNode *node,
+    std::unordered_map<std::string, std::string> &parameters) {
+
+  right_motor = wb_robot_get_device("right wheel motor");
+  left_motor = wb_robot_get_device("left wheel motor");
+
+  wb_motor_set_position(left_motor, INFINITY);
+  wb_motor_set_velocity(left_motor, 0.0);
+
+  wb_motor_set_position(right_motor, INFINITY);
+  wb_motor_set_velocity(right_motor, 0.0);
+
+  cmd_vel_subscription_ = node->create_subscription<geometry_msgs::msg::Twist>(
+      "/cmd_vel", rclcpp::SensorDataQoS().reliable(),
+      std::bind(&MyRobotDriver::cmdVelCallback, this, std::placeholders::_1));
+}
+
+void MyRobotDriver::cmdVelCallback(
+    const geometry_msgs::msg::Twist::SharedPtr msg) {
+  cmd_vel_msg.linear = msg->linear;
+  cmd_vel_msg.angular = msg->angular;
+}
+
+void MyRobotDriver::step() {
+  auto forward_speed = cmd_vel_msg.linear.x;
+  auto angular_speed = cmd_vel_msg.angular.z;
+
+  auto command_motor_left =
+      (forward_speed - angular_speed * HALF_DISTANCE_BETWEEN_WHEELS) /
+      WHEEL_RADIUS;
+  auto command_motor_right =
+      (forward_speed + angular_speed * HALF_DISTANCE_BETWEEN_WHEELS) /
+      WHEEL_RADIUS;
+
+  wb_motor_set_velocity(left_motor, command_motor_left);
+  wb_motor_set_velocity(right_motor, command_motor_right);
+}
+} // namespace my_robot_driver
+
+#include "pluginlib/class_list_macros.hpp"
+PLUGINLIB_EXPORT_CLASS(my_robot_driver::MyRobotDriver,
+                       webots_ros2_driver::PluginInterface)
