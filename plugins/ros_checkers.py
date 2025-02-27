284b46365c5423e087b6697ee082fc55c3fe384e
David V. Lu <davidvlu@gmail.com>
2025-02-27 16:36:41 -0500

    Use sphinx_tamer

--- plugins/ros_checkers.py
+++ plugins/ros_checkers.py
@@ -1,110 +1,29 @@
 #!/usr/bin/python3
 from sphinxlint.checkers import checker
 from sphinxlint.utils import paragraphs
+from sphinx_tamer.sentences import split_into_sentences, register_extra_pattern
+from sphinx_tamer.sentence_scan import is_ignorable
+import pathlib
 import re
-
-# Derived from https://stackoverflow.com/a/31505798
-DOT = '•'
-QUESTION = '✇'
-EXCLAM = '‼'
-SP_LOOKUP = {
-    '.': DOT,
-    '?': QUESTION,
-    '!': EXCLAM,
-}
-STOP = '¶'
-
-# Classes and patterns
-ALPHABETIC = r'([A-Za-z])'
-DIGITS = '([0-9])'
-NOT_BREAK = r'[^!.?]'
-BREAK = r'([!.?])'
-PREFIXES = r'(Mr|St|Mrs|Ms|Dr|etc|vol|cf|et al|vs|eg|Proc|Mon|Tue|Wed|Thu|Fri)\.'
-SUFFIXES = r'(Inc|Ltd|Jr|Sr|Co)'
-WEBSITES = r'\.(com|net|org|io|gov|edu|me|ros)'
-EXTENSIONS = r'\.(xml|cfg|py|launch|frame_id|ini|md|log|h|cpp|bash|patch|gz|yaml|txt|NET|js|msg|srv|action|rst|exe|so|iso|stl|idl)'
-MULTIPLE_DOTS = r'(\.{2,})([^\.])'
-STARTERS = r'(Mr|Mrs|Ms|Dr|Prof|Capt|Cpt|Lt|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)'
-ACRONYMS = r'([A-Z][.][A-Z][.](?:[A-Z][.])?)'
-DIGITS_DOT_DIGITS = re.compile(DIGITS + '([.])' + DIGITS)
-SINGLE_LETTER = re.compile(r'(\s[A-Za-z])(\.)(\s)')
-ON_THE_INSIDE = re.compile(r'()' + BREAK + r'(["\)])')
-DOT_PAREN = re.compile(r'\.( \()')
-
-# RST Formatting Patterns
-HYPERLINK = re.compile(r'(<[^>.!?]*)' + BREAK + r'([^>]*>)')
-BACKTICK = re.compile(r'(`[^`.!?]*)' + BREAK + r'([^`]*`)')
-LIST_PREFIX = re.compile(r'^(\s*[\d#]+)(\.)(.*)')
-TRAILING_FORMATTING = re.compile(r'()' + BREAK + r'(\s*[\*\)]*\s*)$')
+import yaml
 
 
-def split_into_sentences(text):
-    """
-    Split the text into sentences.
-
-    Assumes text does not contain the special characters DOT or STOP
-
-    :param text: text to be split into sentences
-    :type text: str
-
-    :return: list of sentences
-    :rtype: list[str]
-    """
-
-    text = ' ' + text + '  '
-    text = re.sub('Lu!!', f'Lu{EXCLAM}{EXCLAM}', text)
-
-    # Convert nonbreaking punctuation to special characters
-    for pattern in [DIGITS_DOT_DIGITS, SINGLE_LETTER,
-                    HYPERLINK, BACKTICK, LIST_PREFIX, TRAILING_FORMATTING,
-                    ON_THE_INSIDE,
-                    ]:
-        m = pattern.search(text)
-        while m:
-            text = text.replace(m.group(0), m.group(1) + SP_LOOKUP[m.group(2)] + m.group(3))
-            m = pattern.search(text)
-
-    text = re.sub(MULTIPLE_DOTS, lambda match: DOT * len(match.group(1)) + match.group(2), text)
-
-    for pattern in [PREFIXES, SUFFIXES]:
-        text = re.sub(pattern, '\\1' + DOT, text)
-    for pattern in [DOT_PAREN, WEBSITES, EXTENSIONS]:
-        text = re.sub(pattern, DOT + '\\1', text)
-
-    text = re.sub('Ph\.D\.', f'Ph{DOT}D{DOT}', text)
-    text = re.sub('i\.e\.', f'i{DOT}e{DOT}', text)
-
-    text = re.sub('Steven!', f'Steven{EXCLAM}', text)
-    text = re.sub('vd\. Hoorn', f'vd{DOT} Hoorn', text)
-
-    text = re.sub(ACRONYMS + ' ' + STARTERS,
-                  f'\\1{STOP} \\2',
-                  text)
-    text = re.sub(ALPHABETIC + '[.]' + ALPHABETIC + '[.]' + ALPHABETIC + '[.]',
-                  f'\\1{DOT}\\2{DOT}\\3' + DOT,
-                  text
-                 )
-    text = re.sub(ALPHABETIC + '[.]' + ALPHABETIC + '[.]', f'\\1{DOT}\\2{DOT}', text)
-
-    # Convert breaking punctuation to include STOP character
-    # and convert special characters back to normal
-    for stopper, replacement in SP_LOOKUP.items():
-        text = text.replace(stopper, stopper + STOP)
-        text = text.replace(replacement, stopper)
-
-    sentences = text.split(STOP)
-    sentences = [s.strip() for s in sentences]
-
-    return list(filter(None, sentences))
+config_path = pathlib.Path('.sphinx_tamer.yaml')
+ignorable_prefixes = []
+if config_path.exists():
+    config = yaml.safe_load(open(config_path))
+    scan_config = config.get('sentence_scan', {})
+    for pattern_text in scan_config.get('extra_patterns', []):
+        register_extra_pattern(pattern_text)
+    ignorable_prefixes += scan_config.get('ignorable_prefixes', [])
 
 
 @checker('.rst', '.md')
 def check_sentence_count(file, lines, options=None):
-    for paragraph_lno, paragraph in paragraphs(lines):
-        for special_char in [DOT, STOP, QUESTION]:
-            if special_char in paragraph:
-                yield paragraph_lno, f'Contains the special character {special_char}'
+    if is_ignorable(file[7:], ignorable_prefixes):
+        return
 
+    for paragraph_lno, paragraph in paragraphs(lines):
         if paragraph.lstrip().startswith('.. '):
             continue
 
