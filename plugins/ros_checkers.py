1a6c15ff53b51e9ff2631623fb7fde21f1abc303
David V. Lu <davidvlu@gmail.com>
2025-02-27 16:53:27 -0500

    More Sphinx Config

--- plugins/ros_checkers.py
+++ plugins/ros_checkers.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 from sphinxlint.checkers import checker
 from sphinxlint.utils import paragraphs
+from sphinx_tamer import get_lines
+from sphinx_tamer.sphinx_wrapper import load_sphinx, SphinxFile
 from sphinx_tamer.sentences import split_into_sentences, register_extra_pattern
 from sphinx_tamer.sentence_scan import is_ignorable
 import pathlib
@@ -8,7 +10,9 @@ import re
 import yaml
 
 
-config_path = pathlib.Path('.sphinx_tamer.yaml')
+root_path = pathlib.Path('.')
+config_path = root_path / '.sphinx_tamer.yaml'
+src_path = root_path / 'source'
 ignorable_prefixes = []
 if config_path.exists():
     config = yaml.safe_load(open(config_path))
@@ -17,21 +21,36 @@ if config_path.exists():
         register_extra_pattern(pattern_text)
     ignorable_prefixes += scan_config.get('ignorable_prefixes', [])
 
+settings = None
+reporter = None
+parser = None
+
 
 @checker('.rst', '.md')
 def check_sentence_count(file, lines, options=None):
-    if is_ignorable(file[7:], ignorable_prefixes):
+    global settings, reporter, parser, ignorable_prefixes
+
+    if settings is None:
+        settings, reporter, parser = load_sphinx(
+            src_path=src_path,
+            conf_path=root_path
+        )
+
+    file_path = pathlib.Path(file)
+    rel_path = file_path.relative_to(src_path)
+
+    if is_ignorable(str(rel_path), ignorable_prefixes):
         return
 
-    for paragraph_lno, paragraph in paragraphs(lines):
-        if paragraph.lstrip().startswith('.. '):
+    sphinx_file = SphinxFile(file_path, rel_path, settings, reporter, parser)
+
+    for line in get_lines(sphinx_file.parse()):
+        sentences = split_into_sentences(line.get_text())
+        if len(sentences) <= 1:
             continue
 
-        for i, line in enumerate(paragraph.split('\n')):
-            sentences = split_into_sentences(line)
-            if len(sentences) <= 1:
-                continue
-
-            sentence0_words = ' '.join(sentences[0].split(' ')[-3:])
-            sentence1_words = ' '.join(sentences[1].split(' ')[:3])
-            yield paragraph_lno + i, f'Each sentence must start on a new line. Break between "{sentence0_words}" and "{sentence1_words}"'
+        src_sentences = line.get_source_sentences(sentences)
+        for first, second in zip(src_sentences, src_sentences[1:]):
+            words0 = ' '.join(first.split()[-3:])  # last three words
+            words1 = ' '.join(second.split()[:3])  # first three words
+            yield line.line_num, f'Each sentence must start on a new line. Break between "{words0}" and "{words1}"'
