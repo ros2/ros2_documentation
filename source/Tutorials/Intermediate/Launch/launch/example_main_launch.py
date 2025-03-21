37f0a375ce199a34c7f2724a4cf820d780c3ab2a
Emerson Knapp <emerson.b.knapp@gmail.com>
2025-03-21 14:12:09 -0700

    pull launch scripts out of using-substitutions
    Signed-off-by: Emerson Knapp <emerson.b.knapp@gmail.com>

---
+++ source/Tutorials/Intermediate/Launch/launch/example_main_launch.py
@@ -0,0 +1,29 @@
+from launch_ros.substitutions import FindPackageShare
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution, TextSubstitution
+
+
+def generate_launch_description():
+    colors = {
+        'background_r': '200'
+    }
+
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('launch_tutorial'),
+                    'launch',
+                    'example_substitutions_launch.py'
+                ])
+            ]),
+            launch_arguments={
+                'turtlesim_ns': 'turtlesim2',
+                'use_provided_red': 'True',
+                'new_background_r': TextSubstitution(text=str(colors['background_r']))
+            }.items()
+        )
+    ])
