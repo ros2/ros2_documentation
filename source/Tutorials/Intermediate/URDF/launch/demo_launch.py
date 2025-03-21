505d2017c38df522afe22c3fa4796f48f34cf3a8
Emerson Knapp <emerson.b.knapp@gmail.com>
2025-03-21 14:58:48 -0700

    Pull launchfiles out of urdf tutorials'
    Signed-off-by: Emerson Knapp <emerson.b.knapp@gmail.com>

---
+++ source/Tutorials/Intermediate/URDF/launch/demo_launch.py
@@ -0,0 +1,36 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
+    urdf_file_name = 'r2d2.urdf.xml'
+    urdf = os.path.join(
+        get_package_share_directory('urdf_tutorial_r2d2'),
+        urdf_file_name)
+    with open(urdf, 'r') as infp:
+        robot_desc = infp.read()
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'),
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time, 'robot_description': robot_desc}],
+            arguments=[urdf]),
+        Node(
+            package='urdf_tutorial_r2d2',
+            executable='state_publisher',
+            name='state_publisher',
+            output='screen'),
+    ])
