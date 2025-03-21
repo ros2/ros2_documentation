5e5ff403750b8bfa57a8a778fc04bf90a3a95c8d
Emerson Knapp <emerson.b.knapp@gmail.com>
2025-03-21 14:59:14 -0700

    Pulll launchfiles out of beginner tutorials
    Signed-off-by: Emerson Knapp <emerson.b.knapp@gmail.com>

---
+++ source/Tutorials/Beginner-Client-Libraries/launch/cpp_parameters_launch.py
@@ -0,0 +1,16 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package="cpp_parameters",
+            executable="minimal_param_node",
+            name="custom_minimal_param_node",
+            output="screen",
+            emulate_tty=True,
+            parameters=[
+                {"my_parameter": "earth"}
+            ]
+        )
+    ])
