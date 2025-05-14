2d2197b56a4dead6751704c081db57985a55d228
Mario Dominguez <mariodominguez@eprosima.com>
2025-05-14 14:49:45 +0200

    Apply fgallegosalido's review
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>

--- source/Tutorials/Advanced/Topic-Keys/resources/Basic/launch/multiple_topic_sensors_launch.py
+++ source/Tutorials/Advanced/Topic-Keys/resources/Basic/launch/multiple_topic_sensors_launch.py
@@ -1,4 +1,5 @@
-# Copyright 2024 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+# Copyright 2025 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+# Copyright 2025 Open Source Robotics Foundation, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
