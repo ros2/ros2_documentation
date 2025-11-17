569b0639fa34181af27bdc8b59a42a165be87e60
lsy3 <sylukewicent@gmail.com>
2025-11-18 05:18:42 +1100

    docs: demonstrate args vs ros_args in launch file tutorial (#6011)
    Signed-off-by: Luke Sy <sylukewicent@gmail.com>

--- source/Tutorials/Intermediate/Launch/launch/turtlesim_mimic_launch.py
+++ source/Tutorials/Intermediate/Launch/launch/turtlesim_mimic_launch.py
@@ -8,13 +8,15 @@ def generate_launch_description():
             package='turtlesim',
             namespace='turtlesim1',
             executable='turtlesim_node',
-            name='sim'
+            name='sim',
+            arguments=['--ros-args', '--log-level', 'info']
         ),
         Node(
             package='turtlesim',
             namespace='turtlesim2',
             executable='turtlesim_node',
-            name='sim'
+            name='sim',
+            ros_arguments=['--log-level', 'warn']
         ),
         Node(
             package='turtlesim',
