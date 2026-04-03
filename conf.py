cf54876c1a589cc7728b735f1df7e8812a4e456a
root <lakhmanisahil8@gmail.com>
2026-04-03 13:39:31 +0000

    Fix html_static_path warning by anchoring path to conf.py location (#6071)

--- conf.py
+++ conf.py
@@ -173,7 +173,7 @@ html_favicon = 'favicon.ico'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = [os.path.abspath('source/_static')]
+html_static_path = [os.path.join(os.path.dirname(os.path.abspath(__file__)), 'source', '_static')]
 
 # Drop any source link suffix
 html_sourcelink_suffix = ''
